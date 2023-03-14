5649c84a2206319d113ddf044d5d4b0a5b6de3a1
Qwiple <qwiple51@gmail.com>
2023-03-14 19:27:53 +0500

    finish

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from todoapp.views import view_todo, addToDoView, deleteToDoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view_todo, name='todolist'),
+    path('addToDoItem/', addToDoView, name='add_item'),
+    path('deleteTodoItem/<int:item>/', deleteToDoView, name='delete_item')
 ]
--- ToDoList/todoapp/views.py
+++ ToDoList/todoapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import ToDoListItem
 
-# Create your views here.
+def view_todo(request):
+    all_todo_items = ToDoListItem.objects.all()
+    return(render(request, 'todolist.html', {'all_items': all_todo_items}))
+
+def addToDoView(request):
+    content = request.POST['content']
+    new_item = ToDoListItem(content=content)
+    new_item.save()
+    return HttpResponseRedirect('/')
+
+def deleteToDoView(request, item):
+    delete_item = ToDoListItem.objects.get(id=item)
+    delete_item.delete()
+    return HttpResponseRedirect('/')
